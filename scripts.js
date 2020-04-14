44f4bf42b91890aab26b1afcd2f05a9e67c21ca6
mikaylazwerdling <zwerm617@newschool.edu>
2020-04-14 14:16:09 -0400

    moodboard exercise

--- scripts.js
+++ scripts.js
@@ -1,4 +1,40 @@
 // This is where the javascript will live.
 // Using Chrome developer tools, if you look at the console, you can see the message appearing on the console.
 
-console.log('Is the scripts file working?');
+$( function() {
+    $( ".draggable" ).draggable({
+        stack: ".draggable",
+        containment: ".container"
+    });
+
+    // toggle switch
+    $(".switch").click( function(){
+        $(".container").toggleClass("darkmode");
+    })
+
+    // image appear on hover
+    $(".hoverme").mouseover( function(){
+        $(".no").css("display", "block");
+    })
+
+        // image appear on mouse out
+    $(".hoverme").mouseout( function(){
+        $(".no").css("display", "none");
+    })
+
+    // display hidden text
+    $(".clickme").click( function(){
+        $(".extra").fadeToggle("hide");
+    })
+
+    //animate circle
+    $(".circle").mouseover( function(){
+         $(".circle").animate({
+             backgroundColor: "red",
+             width: "250px",
+             height: "250px",
+
+         });
+     })
+
+  } );
